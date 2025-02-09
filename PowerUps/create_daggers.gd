8aed92b10c9f98f3324c6e25fece4b4c0fda847b
Shadman Syed <shadman3d@gmail.com>
2025-02-08 23:03:43 -0500

    better solution to angles, because i remembered the bounds of atan lmfao

--- PowerUps/create_daggers.gd
+++ PowerUps/create_daggers.gd
@@ -10,16 +10,9 @@ func add_blade(powerup: Node2D, position: Vector2, normal: Vector2):
     var blade_scene: PackedScene = spawnable_blades.pick_random()
     var new_blade: Node2D = blade_scene.instantiate()
     new_blade.position = position
-    var angle = abs(atan2(normal.y, normal.x))
+    var angle = atan2(normal.y, normal.x)
     var vel_dir: Vector2
-    if 0 < angle and angle <= PI/2:
-        vel_dir = Vector2(cos(3*PI/4), sin(3*PI/4))
-    elif PI/2 < angle and angle <= PI:
-        vel_dir = Vector2(cos(5*PI/4), sin(5*PI/4))
-    elif PI < angle and angle <= 3*PI/2:
-        vel_dir = Vector2(cos(7*PI/4), sin(7*PI/4))
-    elif 3*PI/2 < angle and angle < TAU:
-        vel_dir = Vector2(cos(PI/4), sin(PI/4))
+    vel_dir = Vector2(cos(angle-PI/4), sin(angle-PI/4))
     powerup.get_parent().add_child(new_blade)
     if new_blade.has_method("set_vel_dir"):
         new_blade.set_vel_dir(vel_dir)
